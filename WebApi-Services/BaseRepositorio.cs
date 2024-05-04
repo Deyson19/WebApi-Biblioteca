719ce92d4510ac17a6432b5b33a1e7a4698ce216
Deyson Venté <deiandresvent@hotmail.com>
2024-05-04 17:48:01 -0500

    Interface generica como repositorio

---
+++ WebApi-Services/BaseRepositorio.cs
@@ -0,0 +1,11 @@
+﻿
+using WebApi_Helpers;
+
+namespace WebApi_Services
+{
+    public interface BaseRepositorio<T>
+    {
+        public Task<Respuesta<T>> Create(T entity);
+        public Task<Respuesta<IEnumerable<T>>> GetAll();
+    }
+}
