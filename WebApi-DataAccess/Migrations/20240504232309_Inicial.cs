3b2697fe4827ea266ae3e83dfe6314414d93531e
Deyson Venté <deiandresvent@hotmail.com>
2024-05-04 18:24:05 -0500

    Db montada - migraciones ok

---
+++ WebApi-DataAccess/Migrations/20240504232309_Inicial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Isbn = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    IdentificacionUsuario = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    FechaPrestamo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaMaximaDevolucion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoUsuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoUsuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+
+            migrationBuilder.DropTable(
+                name: "TipoUsuarios");
+        }
+    }
+}
