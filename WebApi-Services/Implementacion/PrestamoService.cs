53971eca95d20dc759d65360397546e34332409d
Deyson Venté <deiandresvent@hotmail.com>
2024-05-04 17:54:02 -0500

    Contratos implementados.

---
+++ WebApi-Services/Implementacion/PrestamoService.cs
@@ -0,0 +1,79 @@
+﻿
+using WebApi_DataAccess.Entities;
+using WebApi_DataAccess;
+using WebApi_Helpers;
+using WebApi_Services.Contrato;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_Services.Implementacion
+{
+    public class PrestamoService(BibliotecaDbContext context) : IPrestamoService
+    {
+        private readonly BibliotecaDbContext _dbContext = context;
+
+        public async Task<Respuesta<Prestamo>> Create(Prestamo entity)
+        {
+            if (entity == null)
+            {
+                return new Respuesta<Prestamo>
+                {
+                    IsSuccess = false,
+                    Message = "No es un modelo correcto",
+                    Result = entity
+                };
+            }
+            await _dbContext.Prestamos.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return new Respuesta<Prestamo>
+            {
+                IsSuccess = true,
+                Message = "Registro creado",
+                Result = entity
+            };
+        }
+
+        public async Task<Respuesta<IEnumerable<Prestamo>>> GetAll()
+        {
+            var listado = await _dbContext.Prestamos.ToListAsync();
+            return new Respuesta<IEnumerable<Prestamo>>
+            {
+                IsSuccess = listado.Any(),
+                Result = listado,
+                Message = listado.Any() ? "Listado de prestamos" : "No hay resultados"
+            };
+        }
+
+        public async Task<Respuesta<Prestamo>> GetById(int id)
+        {
+            if (id == 0)
+            {
+                return new Respuesta<Prestamo>
+                {
+                    IsSuccess = false,
+                    Message = "El valor del id no es correcto",
+                    Result = null
+                };
+            }
+            var prestamo = await GetPrestamo(id);
+            return new Respuesta<Prestamo>
+            {
+                IsSuccess = prestamo != null,
+                Message = prestamo != null ? "Se ha encontrado un prestamo" : "No hay resultados",
+                Result = prestamo
+            };
+        }
+
+
+        public async Task<bool> UsuarioTienePrestamo(string idUsuario)
+        {
+            var prestamo = await _dbContext.Prestamos.Where(x => x.FechaPrestamo > DateTime.Now).FirstOrDefaultAsync(x => x.IdentificacionUsuario.ToLower() == idUsuario.ToLower());
+            return prestamo != null;
+        }
+
+        private async Task<Prestamo> GetPrestamo(int id)
+        {
+            var prestamo = await _dbContext.Prestamos.FirstOrDefaultAsync(x => x.Id == id);
+            return prestamo;
+        }
+    }
+}
---
+++ WebApi-Services/Implementacion/TipoUsuarioService.cs
@@ -0,0 +1,61 @@
+﻿
+using WebApi_DataAccess.Entities;
+using WebApi_DataAccess;
+using WebApi_Helpers;
+using WebApi_Services.Contrato;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_Services.Implementacion
+{
+    public class TipoUsuarioService(BibliotecaDbContext context) : ITipoUsuarioService
+    {
+        private readonly BibliotecaDbContext _dbContext = context;
+
+        public async Task<Respuesta<TipoUsuario>> Create(TipoUsuario entity)
+        {
+            if (entity == null)
+            {
+                return new Respuesta<TipoUsuario>
+                {
+                    IsSuccess = false,
+                    Message = "El modelo no es correcto",
+                    Result = entity
+                };
+            }
+            else
+            {
+                await _dbContext.TipoUsuarios.AddAsync(entity);
+                await _dbContext.SaveChangesAsync();
+
+                return new Respuesta<TipoUsuario>
+                {
+                    IsSuccess = true,
+                    Message = "Registro creado",
+                    Result = entity
+                };
+            }
+        }
+
+        public async Task<Respuesta<IEnumerable<TipoUsuario>>> GetAll()
+        {
+            var listado = await _dbContext.TipoUsuarios.ToListAsync();
+            return new Respuesta<IEnumerable<TipoUsuario>>
+            {
+                IsSuccess = listado.Count != 0,
+                Result = listado,
+                Message = listado.Count != 0 ? "Listado de tipos de usuario" : "No hay registros"
+            };
+        }
+
+        public async Task<Respuesta<TipoUsuario>> GetById(int id)
+        {
+            var tipoUsuario = await _dbContext.TipoUsuarios.FirstOrDefaultAsync(x => x.Id == id);
+            return new Respuesta<TipoUsuario>
+            {
+                IsSuccess = tipoUsuario != null,
+                Message = tipoUsuario != null ? "Tipo de usuario encontrado" : "No hay coincidencias",
+                Result = tipoUsuario
+            };
+        }
+    }
+}
