a340672d40e6d699c5ffff94ab5a5ba9616a1138
Deyson Venté <deiandresvent@hotmail.com>
2024-05-04 20:37:06 -0500

    Migraciones de la tabla referenciada

--- WebApi-DataAccess/Migrations/20240505004141_Relacion_Prestamo_TipoUsuario.Designer.cs
+++ WebApi-DataAccess/Migrations/20240505004141_Relacion_Prestamo_TipoUsuario.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi_DataAccess;
 
@@ -11,9 +12,11 @@ using WebApi_DataAccess;
 namespace WebApi_DataAccess.Migrations
 {
     [DbContext(typeof(BibliotecaDbContext))]
-    partial class BibliotecaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240505004141_Relacion_Prestamo_TipoUsuario")]
+    partial class Relacion_Prestamo_TipoUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebApi-DataAccess/Migrations/20240505004141_Relacion_Prestamo_TipoUsuario.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Relacion_Prestamo_TipoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TipoUsuarioId",
+                table: "Prestamos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamos_TipoUsuarioId",
+                table: "Prestamos",
+                column: "TipoUsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prestamos_TipoUsuarios_TipoUsuarioId",
+                table: "Prestamos",
+                column: "TipoUsuarioId",
+                principalTable: "TipoUsuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prestamos_TipoUsuarios_TipoUsuarioId",
+                table: "Prestamos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Prestamos_TipoUsuarioId",
+                table: "Prestamos");
+
+            migrationBuilder.DropColumn(
+                name: "TipoUsuarioId",
+                table: "Prestamos");
+        }
+    }
+}
