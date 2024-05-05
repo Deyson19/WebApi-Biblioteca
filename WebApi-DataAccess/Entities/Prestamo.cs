2c6e76e01feb8554f77fea9361e208959a6bb195
Deyson Venté <deiandresvent@hotmail.com>
2024-05-04 20:35:49 -0500

    Referenciar la tabla tipoUsuario

--- WebApi-DataAccess/Entities/Prestamo.cs
+++ WebApi-DataAccess/Entities/Prestamo.cs
@@ -12,12 +12,16 @@ namespace WebApi_DataAccess.Entities
         public string IdentificacionUsuario { get; set; }
         public required DateTime FechaPrestamo { get; set; }
         public required DateTime FechaMaximaDevolucion { get; set; }
+        public int TipoUsuarioId { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
     }
     public class CrearPrestamoViewModel
     {
         [MaxLength(6, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
         public string Isbn { get; set; }
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
         public string IdentificacionUsuario { get; set; }
+        //[MaxLength(1, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
         public int TipoUsuarioId { get; set; }
     }
     public class ObtenerPrestamoViewModel
@@ -26,5 +30,13 @@ namespace WebApi_DataAccess.Entities
         public string Isbn { get; set; }
         public string IdentificacionUsuario { get; set; }
         public DateTime FechaMaximaDevolucion { get; set; }
+    }
+    public class ObtenerPrestamoPorId
+    {
+        public int Id { get; set; }
+        public string Isbn { get; set; }
+        public string IdentificacionUsuario { get; set; }
+        public int TipoUsuario { get; set; }
+        public string FechaMaximaDevolucion { get; set; }
     }
 }
